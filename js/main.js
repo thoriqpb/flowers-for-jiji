a96523f09d7b97123eeafc92b4e3f784fdec8177
Thoriq Putra Belligan <utasafa22@gmail.com>
2025-08-07 05:51:46 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
 
-    const titles = ('I LOVE U NDUT').split('')
+    const titles = ('GET WELL SOON NDUTT<3').split('')
     const titleElement = document.getElementById('title');
     let index = 0;
 
